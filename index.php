afbe45c9ae15cc06e3ab3122ed341053ea620a0f
Jonathon Squire <jgsquire27@gmail.com>
2022-12-07 12:18:17 -0700

    add favicons, created calculator, sum not fixed

--- index.php
+++ index.php
@@ -5,10 +5,29 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel='icon' type='image/x-icon' href='/images/favicon.ico'>
+  <link rel='stylesheet' href='style.css'>
 </head>
 <body>
-  <p>kjkd slkdlskf jlsj fo sifj ek sfo sdkf os fjosj eoifjdsokjkfosdj fo sifejs o dfks jfie </p>
-  
+
+
+
+
+
+  <form action="./classes/functions.php" method="get">
+    <input type="text" name="num01" placeholder="Number 1">
+    <select name="oper">
+      <label>Choose operations!</label>
+      <option value='add'>add</option>
+      <option value='sub'>subtract</option>
+    </select>
+    <input type='text' name='num02' placeholder='number 2'>
+    <button type='submit'>calculate</button>
+  </form>
+
+
+
+
   
 </body>
 </html>
